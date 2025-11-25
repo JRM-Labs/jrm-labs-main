76248cff8afae8b6697619a5f73b7aba3be299d0
christopher-harris <ccharris0607@outlook.com>
2025-11-25 14:14:10 -0600

    Removes unused HomeAboutSectionComponent from HomePage.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,7 +3,6 @@ import {ContactFormComponent} from "../contact-form/contact-form.component";
 import {HomePageServicesComponent} from "./home-page-services/home-page-services.component";
 import {HomePageHeroComponent} from "./home-page-hero/home-page-hero.component";
 import {HomePageApplicationsComponent} from "./home-page-applications/home-page-applications.component";
-import {HomeAboutSectionComponent} from "./home-about-section/home-about-section.component";
 
 @Component({
   selector: 'app-home',
@@ -12,7 +11,6 @@ import {HomeAboutSectionComponent} from "./home-about-section/home-about-section
     HomePageServicesComponent,
     HomePageHeroComponent,
     HomePageApplicationsComponent,
-    HomeAboutSectionComponent,
   ],
   templateUrl: './home.page.html',
   styleUrl: './home.page.scss'
